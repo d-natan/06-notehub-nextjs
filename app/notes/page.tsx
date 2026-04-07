122ab279f4b2852e6cd63b065c55cb45009ccd66
natan <ochenoficialniy@gmail.com>
2026-04-07 19:48:15 +0300

    some new changes

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,4 +1,8 @@
-import NotesClient from "./Notes.client";
+import dynamic from "next/dynamic";
+
+const NotesClient = dynamic(() => import("./Notes.client"), {
+  ssr: false,
+});
 
 export default function NotesPage() {
   return <NotesClient />;
